0b807d9c52e09114d63d0006bdebfba24e8b8b42
Vlad <anikanovvlad@yandex.ru>
2022-02-12 22:44:14 +0500

    main menu

--- app/src/main/java/com/example/sensor_project/MainMenu.java
+++ app/src/main/java/com/example/sensor_project/MainMenu.java
@@ -6,16 +6,19 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,14 +31,30 @@ import java.util.ArrayList;
 
 public class MainMenu extends AppCompatActivity {
 
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainmenu);
         Resources r = this.getResources();
-        int px = (int) TypedValue.applyDimension(
+        int size_5_dp = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                5,
+                r.getDisplayMetrics()
+        );
+        int size_10_dp = (int) TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
                 10,
+                r.getDisplayMetrics()
+        );
+        int size_40_dp = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                40,
+                r.getDisplayMetrics()
+        );
+        int size_60_dp = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                60,
                 r.getDisplayMetrics()
         );
 
@@ -78,10 +97,76 @@ public class MainMenu extends AppCompatActivity {
                 sensors.add(Integer.parseInt(aaaa));
             }
         }
-//
         for (int id_sensor: sensors){
-            // добавление сенсоров
-            System.out.println(id_sensor);
+            String name = a.doInBackground("get_sensor_name", Integer.toString(id_sensor));
+            String data = a.doInBackground("get_data_sensor", Integer.toString(id_sensor));
+
+            LinearLayout linLayout = new LinearLayout(getApplicationContext());
+            linLayout.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams linLayoutParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, size_60_dp);
+            linLayoutParam.topMargin = size_10_dp;
+            linLayout.setLayoutParams(linLayoutParam);
+            linLayout.setBackgroundResource(R.drawable.gray_sensor);
+            linLayout.setGravity(Gravity.CENTER_VERTICAL);
+
+            View status = new View(getApplicationContext());
+            LinearLayout.LayoutParams status_param = new LinearLayout.LayoutParams(size_40_dp, size_40_dp);
+            status_param.leftMargin = size_10_dp;
+            status.setLayoutParams(status_param);
+            switch (data){
+                case "gray":
+                    status.setBackgroundResource(R.drawable.gray);
+                    break;
+                case "red":
+                    status.setBackgroundResource(R.drawable.red);
+                    break;
+                case "yellow":
+                    status.setBackgroundResource(R.drawable.yellow);
+                    break;
+                case "green":
+                    status.setBackgroundResource(R.drawable.green);
+                    break;
+            }
+
+            TextView name_view = new TextView(getApplicationContext());
+            LinearLayout.LayoutParams name_view_params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, size_40_dp);
+            name_view_params.leftMargin = size_10_dp;
+            name_view.setLayoutParams(name_view_params);
+            name_view.setText(name);
+            name_view.setTextColor(Color.rgb(0, 0, 0));
+            name_view.setGravity(Gravity.START | Gravity.CENTER_VERTICAL);
+            name_view.setTextSize(22);
+
+            LinearLayout layout_for_edit = new LinearLayout(getApplicationContext());
+            layout_for_edit.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams layout_for_edit_param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+            layout_for_edit.setLayoutParams(layout_for_edit_param);
+            layout_for_edit.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
+
+            View status1 = new View(getApplicationContext());
+            LinearLayout.LayoutParams status_param1 = new LinearLayout.LayoutParams(size_40_dp, size_40_dp);
+            status_param1.rightMargin = size_10_dp;
+            status1.setLayoutParams(status_param1);
+            switch (data){
+                case "gray":
+                    status1.setBackgroundResource(R.drawable.gray);
+                    break;
+                case "red":
+                    status1.setBackgroundResource(R.drawable.red);
+                    break;
+                case "yellow":
+                    status1.setBackgroundResource(R.drawable.yellow);
+                    break;
+                case "green":
+                    status1.setBackgroundResource(R.drawable.green);
+                    break;
+            }
+
+            linLayout.addView(status);
+            linLayout.addView(name_view);
+            layout_for_edit.addView(status1);
+            linLayout.addView(layout_for_edit);
+            mainlayout.addView(linLayout);
         }
         } catch (Exception ex){
             Toast.makeText(getApplicationContext(), R.string.nobody_knows, Toast.LENGTH_SHORT).show();
@@ -266,8 +351,10 @@ public class MainMenu extends AppCompatActivity {
         @Override
         protected String doInBackground(String... arg) {
             String url;
-            if (arg[0].equals("get_sensors")){
+            if (arg[0].equals("get_sensors")) {
                 url = "https://watersensors.herokuapp.com" + "/get_sensors?i=" + arg[1];
+            } else if (arg[0].equals("get_sensor_name")) {
+                url = "https://watersensors.herokuapp.com" + "/get_sensor_name?i=" + arg[1];
             } else {
                 url = "https://watersensors.herokuapp.com" + "/get_data_sensor?i=" + arg[1];
             }
